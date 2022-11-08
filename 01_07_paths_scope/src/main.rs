e71b7156e8763cc9f69692ab92c01838a3ca0333
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-08 04:56:30 +0100

    The usefulness of the 'use' keyword

--- 01_07_paths_scope/src/main.rs
+++ 01_07_paths_scope/src/main.rs
@@ -1,7 +1,44 @@
+/* Explanation:
+ * To avoid having to write out a full path
+ * every time we need to call a casual greeting
+ * function, we can bring the formal submodule
+ * into scope for this [source file] with a [use]
+ * statement */
+
+use greeting::casual;
+
 fn main() {
-    println!("Hello, world!");
+    /* Explanation:
+     * We use a relative path to access the english
+     * function from within the formal submodule of
+     * the greeting module.
+     *
+     * */
+
+    greeting::casual::english();
+
+    casual::english();
+    casual::spanish();
 }
 
 mod greeting {
-    pub mod formal {}
+    pub mod formal {
+        pub fn _english() {
+            println!("hello");
+        }
+
+        pub fn _spanish() {
+            println!("hola");
+        }
+    }
+
+    pub mod casual {
+        pub fn english() {
+            println!("hey");
+        }
+
+        pub fn spanish() {
+            println!("oye");
+        }
+    }
 }
