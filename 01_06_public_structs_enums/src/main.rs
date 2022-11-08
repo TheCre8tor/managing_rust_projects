d379dd857621c5fdbcb60826a5df5483bd1f93b6
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-08 04:28:59 +0100

    Public structs and enums

--- 01_06_public_structs_enums/src/main.rs
+++ 01_06_public_structs_enums/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let rect = shape::Rectangle::new(3.0, 5.0);
+    println!("rect area is {}", rect.get_area());
+    println!("rect width is {}", rect.width);
+}
+
+mod shape {
+    pub struct Rectangle {
+        pub width: f64,
+        height: f64,
+    }
+
+    // Implementation block ->
+
+    impl Rectangle {
+        pub fn new(width: f64, height: f64) -> Rectangle {
+            Rectangle { width, height }
+        }
+
+        pub fn get_area(&self) -> f64 {
+            self.width * self.height
+        }
+    }
 }
